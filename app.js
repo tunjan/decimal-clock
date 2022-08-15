218be3e9c5425b91a3b6f29f0379622df5255988
tunjan <trigo@inventati.org>
2022-08-15 10:50:17 +0200

    changed css

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ function currentTime() {
   let formattedDate = dateFormatter(toDecimal(dateNow));
   let elapsedPercent = Math.trunc(toDecimal(dateNow)/10); //output -> number between 0 and 100
   let elem = document.getElementById("Bar");
+
   elem.style.width = elapsedPercent  + "%";
   elem.innerHTML = elapsedPercent + "%";
 
@@ -12,7 +13,7 @@ function currentTime() {
 
 function toDecimal(date) {
   let dateToHour = date.getHours() + date.getMinutes()/60 + date.getSeconds()/3600 +  date.getMilliseconds()/3600000;
-  let hourToDecimal = toHour * (1000/24);
+  let hourToDecimal = dateToHour * (1000/24);
   return hourToDecimal;
 }
 
